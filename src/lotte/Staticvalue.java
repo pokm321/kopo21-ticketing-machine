06f77b6f1b92583546a13602bebb047f9d42c9af
pokm321 <pokm90@gmail.com>
2022-04-20 14:01:44 +0900

    edit

--- src/lotte/Staticvalue.java
+++ src/lotte/Staticvalue.java
@@ -1,146 +1,29 @@
 package lotte;
 
 public final class Staticvalue {
-	private static int DAYORNIGHT_DAY = 1;
-	private static int DAYORNIGHT_NIGHT = 2;
+	public static final int DAYORNIGHT_DAY = 1;
+	public static final int DAYORNIGHT_NIGHT = 2;
 	
-	private static int TYPE_ALL = 1;
-	private static int TYPE_PARK = 2;
+	public static final int TYPE_ALL = 1;
+	public static final int TYPE_PARK = 2;
 	
-	private static int SSN_T_MAX = 4;
-	private static int SSN_T_MIN = 1;
+	public static final int SSN_T_MAX = 4;
+	public static final int SSN_T_MIN = 1;
 	
-	private static int AGE_BABY = 0;
-	private static int AGE_CHILD = 1;
-	private static int AGE_TEEN = 2;
-	private static int AGE_ADULT = 3;
-	private static int AGE_ELDER = 4;
+	public static final int AGE_BABY = 0;
+	public static final int AGE_CHILD = 1;
+	public static final int AGE_TEEN = 2;
+	public static final int AGE_ADULT = 3;
+	public static final int AGE_ELDER = 4;
 	
-	private static int DISCOUNT_MAX = 6;
-	private static int DISCOUNT_MIN = 1;
+	public static final int DISCOUNT_MAX = 6;
+	public static final int DISCOUNT_MIN = 1;
 	
-	private static int DISCOUNT_NONE = 1;
-	private static int DISCOUNT_DISABLED = 2;
-	private static int DISCOUNT_VETERAN = 3;
-	private static int DISCOUNT_SOLDIER = 4;
-	private static int DISCOUNT_PREGNANT = 5;
-	private static int DISCOUNT_MULTICHILDS = 6;
-	
-	
-	// getters and setters
-	public static int getDAYORNIGHT_DAY() {
-		return DAYORNIGHT_DAY;
-	}
-	public static void setDAYORNIGHT_DAY(int dAYORNIGHT_DAY) {
-		DAYORNIGHT_DAY = dAYORNIGHT_DAY;
-	}
-	public static int getDAYORNIGHT_NIGHT() {
-		return DAYORNIGHT_NIGHT;
-	}
-	public static void setDAYORNIGHT_NIGHT(int dAYORNIGHT_NIGHT) {
-		DAYORNIGHT_NIGHT = dAYORNIGHT_NIGHT;
-	}
-	public static int getTYPE_ALL() {
-		return TYPE_ALL;
-	}
-	public static void setTYPE_ALL(int tYPE_ALL) {
-		TYPE_ALL = tYPE_ALL;
-	}
-	public static int getTYPE_PARK() {
-		return TYPE_PARK;
-	}
-	public static void setTYPE_PARK(int tYPE_PARK) {
-		TYPE_PARK = tYPE_PARK;
-	}
-	public static int getSSN_T_MIN() {
-		return SSN_T_MIN;
-	}
-	public static void setSSN_T_MIN(int sSN_T_MIN) {
-		SSN_T_MIN = sSN_T_MIN;
-	}
-	public static int getSSN_T_MAX() {
-		return SSN_T_MAX;
-	}
-	public static void setSSN_T_MAX(int sSN_T_MAX) {
-		SSN_T_MAX = sSN_T_MAX;
-	}
-	public static int getAGE_BABY() {
-		return AGE_BABY;
-	}
-	public static void setAGE_BABY(int aGE_BABY) {
-		AGE_BABY = aGE_BABY;
-	}
-	public static int getAGE_CHILD() {
-		return AGE_CHILD;
-	}
-	public static void setAGE_CHILD(int aGE_CHILD) {
-		AGE_CHILD = aGE_CHILD;
-	}
-	public static int getAGE_TEEN() {
-		return AGE_TEEN;
-	}
-	public static void setAGE_TEEN(int aGE_TEEN) {
-		AGE_TEEN = aGE_TEEN;
-	}
-	public static int getAGE_ADULT() {
-		return AGE_ADULT;
-	}
-	public static void setAGE_ADULT(int aGE_ADULT) {
-		AGE_ADULT = aGE_ADULT;
-	}
-	public static int getAGE_ELDER() {
-		return AGE_ELDER;
-	}
-	public static void setAGE_ELDER(int aGE_ELDER) {
-		AGE_ELDER = aGE_ELDER;
-	}
-	public static int getDISCOUNT_MIN() {
-		return DISCOUNT_MIN;
-	}
-	public static void setDISCOUNT_MIN(int dISCOUNT_MIN) {
-		DISCOUNT_MIN = dISCOUNT_MIN;
-	}
-	public static int getDISCOUNT_MAX() {
-		return DISCOUNT_MAX;
-	}
-	public static void setDISCOUNT_MAX(int dISCOUNT_MAX) {
-		DISCOUNT_MAX = dISCOUNT_MAX;
-	}
-	public static int getDISCOUNT_DISABLED() {
-		return DISCOUNT_DISABLED;
-	}
-	public static void setDISCOUNT_DISABLED(int dISCOUNT_DISABLED) {
-		DISCOUNT_DISABLED = dISCOUNT_DISABLED;
-	}
-	public static int getDISCOUNT_NONE() {
-		return DISCOUNT_NONE;
-	}
-	public static void setDISCOUNT_NONE(int dISCOUNT_NONE) {
-		DISCOUNT_NONE = dISCOUNT_NONE;
-	}
-	public static int getDISCOUNT_SOLDIER() {
-		return DISCOUNT_SOLDIER;
-	}
-	public static void setDISCOUNT_SOLDIER(int dISCOUNT_SOLDIER) {
-		DISCOUNT_SOLDIER = dISCOUNT_SOLDIER;
-	}
-	public static int getDISCOUNT_VETERAN() {
-		return DISCOUNT_VETERAN;
-	}
-	public static void setDISCOUNT_VETERAN(int dISCOUNT_VETERAN) {
-		DISCOUNT_VETERAN = dISCOUNT_VETERAN;
-	}
-	public static int getDISCOUNT_PREGNANT() {
-		return DISCOUNT_PREGNANT;
-	}
-	public static void setDISCOUNT_PREGNANT(int dISCOUNT_PREGNANT) {
-		DISCOUNT_PREGNANT = dISCOUNT_PREGNANT;
-	}
-	public static int getDISCOUNT_MULTICHILDS() {
-		return DISCOUNT_MULTICHILDS;
-	}
-	public static void setDISCOUNT_MULTICHILDS(int dISCOUNT_MULTICHILDS) {
-		DISCOUNT_MULTICHILDS = dISCOUNT_MULTICHILDS;
-	}
+	public static final int DISCOUNT_NONE = 1;
+	public static final int DISCOUNT_DISABLED = 2;
+	public static final int DISCOUNT_VETERAN = 3;
+	public static final int DISCOUNT_SOLDIER = 4;
+	public static final int DISCOUNT_PREGNANT = 5;
+	public static final int DISCOUNT_MULTICHILDS = 6;
 	
 }
