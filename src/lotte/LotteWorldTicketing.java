759072791447ed386795c7a41721907dfab42146
pokm321 <pokm90@gmail.com>
2022-04-21 09:34:22 +0900

    renamed

--- src/lotte/LotteWorldTicketing.java
+++ src/lotte/LotteWorldTicketing.java
@@ -2,7 +2,7 @@ package lotte;
 
 import java.io.IOException;
 
-public class Lotte_World_Ticketing {
+public class LotteWorldTicketing {
 	public static void main(String[] args) {
 		TicketSystem ticketsystem = new TicketSystem();
 		try {
